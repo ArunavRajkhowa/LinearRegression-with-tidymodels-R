3da03c5a2784420e6c48c1c0f6f8cc48abd458b0
ArunavRajkhowa <arunav.rajkhowa2@gmail.com>
2022-08-24 17:16:33 +0530

    data preprocessing done

--- LinearRegrssion_tidymodels.R
+++ LinearRegrssion_tidymodels.R
@@ -49,8 +49,8 @@ fico_fn=function(x){
   
   temp=temp %>% 
     separate(fico,into = c('f1','f2')) %>% 
-    mutate(f1=as.numeric(),
-           f2=as.numeric(),
+    mutate(f1=as.numeric(f1),
+           f2=as.numeric(f2),
            fico = 0.5*(f1+f2) ) %>% 
     select(-f1,-f2)
   
@@ -77,6 +77,49 @@ percent_to_numeric_fn=function(x){
 }
 ###
 
+#now we will use functions recipe(),prep() & bake() of library tidymodels 
+#this will make our data preprocessing code more......tidy
+
+#run the code stepbystep to understand
+dp_pipe = recipe(Interest.Rate ~ .,data=df_train) %>%  
+  # update role of 'to-drop' columns first
+  #update_role(ID,Amount.Funded.By.Investors,new_role = "drop_vars") %>% 
+  # update role of to convert to numeric columns
+  update_role(Amount.Requested,
+              Open.CREDIT.Lines,
+              Revolving.CREDIT.Balance,new_role="to_numeric") %>% 
+  update_role(Home.Ownership,State,Loan.Length,Loan.Purpose,new_role="to_dummies") %>% 
+  
+  
+  #now we drop 'drop_vars'
+  step_rm(has_role('drop_vars')) %>% 
+  
+  #applying the custom functions
+  step_mutate_at(FICO.Range,fn=fico_fn) %>% 
+  step_mutate_at(Employment.Length,fn=emp_len_fn) %>% 
+  step_mutate_at(Debt.To.Income.Ratio,fn=percent_to_numeric_fn) %>% 
+  step_mutate_at(Interest.Rate,fn=percent_to_numeric_fn,skip=TRUE) %>% #only for ld_train
+  step_mutate_at(has_role("to_numeric"),fn=as.numeric) %>% 
+  
+  # creating n-1 dummies for categoricl variables
+  #???????????????????????????????????????????????????????????????????
+  step_unknown(has_role("to_dummies"),new_level="__missing__") %>% 
+  #cleaning & grouping them into __other__ . choice of threshold is subjective
+  step_other(has_role("to_dummies"),threshold =0.02,other="__other__") %>% 
+  #create dummies
+  step_dummy(has_role("to_dummies")) %>% #creates n-1 dummies
+  
+  #imputing values
+  step_impute_median(all_numeric(),-all_outcomes())
 
 
+dp_pipe=prep(dp_pipe)
+
+train=bake(dp_pipe,new_data = NULL)  
+test=bake(dp_pipe,new_data=df_test)
+
+
+vis_dat(train) # lines show the NAs..
+vis_dat(df_train)
+### data preprocessing ends here
 
